3a6d9453de624ca138b6fb4db1d1686856178e46
GeshaAry <gesaary123@gmail.com>
2023-11-07 19:28:25 +0800

    front-end update

---
+++ src/frontend/components/ClaimPage.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { Grid, Button } from '@mui/material';
+import { Typography } from '@mui/material';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import YourIconLogo from '../assets/rose.jpg';
+
+const ClaimPage = () => {
+
+    return (
+        <>
+            <div className='afterscan-page'>
+                <Grid container>
+                    <Navbar />
+                    <Grid item xs={12}>
+                        <h1 className='title'>Honney Coffee</h1>
+                        <div className='title-map'>
+                            <LocationOnIcon style={{ fontSize: 16 }} />
+                            <Typography fontSize="16px" component={'center'}>
+                                Hanoman, Ubud
+                            </Typography>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <h3 className='title'>You're collected 1 NFT</h3>
+                        <Typography fontSize="15px" component={'center'} style={{ paddingLeft: '20px', paddingRight: '20px' }}>
+                            Purchase any product in the store to get an NFT and get a surprise
+                        </Typography>
+                    </Grid>
+                    <Grid container spacing={2} style={{ padding: '20px' }}>
+                        <Grid item xs={4} sm={6} md={4} lg={3}>
+                            <div className="wrap-image-claim">
+                                <img src={YourIconLogo} alt="Image 1" width="100%" />
+                            </div>
+                        </Grid>
+                        <Grid item xs={4} sm={6} md={4} lg={3}>
+                            <div className="wrap-image-claim">
+                                <img src={YourIconLogo} alt="Image 1" width="100%" />
+                            </div>
+                        </Grid>
+                        <Grid item xs={4} sm={6} md={4} lg={3}>
+                            <div className="wrap-image-claim">
+                                <img src={YourIconLogo} alt="Image 1" width="100%" />
+                            </div>
+                        </Grid>
+                        <Grid item xs={4} sm={6} md={4} lg={3}>
+                            <div className="wrap-image-claim">
+                                <img src={YourIconLogo} alt="Image 1" width="100%" />
+                            </div>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center', marginTop:'20px', paddingBottom:'20px' }}>
+                        <Button
+                            variant="contained"
+                            className='button-explore'
+                            onClick={() => {
+                                // Add your share functionality here
+                            }}
+                        >
+                           Claim Your Reward
+                        </Button>
+                    </Grid>
+                </Grid>
+            </div>
+        </>
+    )
+}
+
+export default ClaimPage
---
+++ src/frontend/components/ImageDetailPage.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Grid from '@mui/material/Grid';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { right } from '@popperjs/core';
+import { Button } from '@mui/material';
+import ShareIcon from '@mui/icons-material/Share';
+import YourIconLogo from '../assets/rose.jpg';
+
+const ImageDetailPage = () => {
+    return (
+        <>
+            <div className='imagedetail-page'>
+                <Grid container>
+                    <Navbar />
+                    <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+                        <h2 className='title'>Po Party</h2>
+                    </Grid>
+                    <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+                        <div className='wrap-image-scan'>
+                            <img src={YourIconLogo} alt="" />
+                        </div>
+                    </Grid>
+                    <Grid item xs={6} style={{ paddingRight: '20px', marginTop: '20px' }}>
+                        <p style={{ textAlign: right }}><span style={{ fontWeight: 'bold' }}>Artist</span> : Poppy Lessart</p>
+                    </Grid>
+                    <Grid item xs={6} style={{ paddingLeft: '20px', marginTop: '20px' }}>
+                        <div style={{ display: 'flex' }}>
+                            <LocationOnIcon style={{ marginTop: '10px' }} />
+                            <p>Denpasar, Bali</p>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+                        <p style={{ textAlign: 'justify' }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, quos magni soluta nam dolorum praesentium a rem error molestiae eos voluptatum obcaecati iure nobis labore saepe? Voluptate ea explicabo non, quia doloremque consequuntur. Maxime eos fugit ipsam earum? Quae atque officia totam at quod amet quam accusamus, similique voluptatibus error iusto et aut? Excepturi perferendis fugit sequi fugiat veritatis perspiciatis! Quisquam repellendus nam ipsa odit natus blanditiis obcaecati accusamus doloremque. Iure nam ab repellendus autem ipsum molestias dolor molestiae non?</p>
+                    </Grid>
+                    <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button
+                            variant="contained"
+                            className='button-share'
+                            startIcon={<ShareIcon />}
+                            onClick={() => {
+                                // Add your share functionality here
+                            }}
+                        >
+                            Share
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center', marginTop:'20px', paddingBottom:'20px' }}>
+                        <Button
+                            variant="contained"
+                            className='button-explore'
+                            onClick={() => {
+                                // Add your share functionality here
+                            }}
+                        >
+                           Explore Poppy's Art & Collaborations
+                        </Button>
+                    </Grid>
+                </Grid>
+            </div>
+        </>
+    )
+}
+
+export default ImageDetailPage
