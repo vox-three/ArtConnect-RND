f761cada8c89298fa9d14f8c42054b2dd7b2ab4b
Gesha Ary <gesaary123@gmail.com>
2023-11-14 19:34:28 +0800

    add transaction list and change collection page

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import 'aos/dist/aos.css'; // Impor file CSS AOS
+import AOS from 'aos';
+AOS.init();
 
 ReactDOM.render(
     <React.StrictMode>
--- src/frontend/pages/Claim/index.tsx
+++ src/frontend/pages/Claim/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Grid, Button } from '@mui/material';
-import { Typography } from '@mui/material';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import YourIconLogo from '../../assets/rose.jpg';
+import StarsIcon from '@mui/icons-material/Stars';
 import BottomBar from "../../components/BottomBar";
 import Navbar from "../../components/Navbar";
 
@@ -15,52 +13,13 @@ const ClaimPage = () => {
             <div className='afterscan-page'>
                 <Grid container>
                     <Grid item xs={12}>
-                        <h1 className='title'>Honney Coffee</h1>
-                        <div className='title-map'>
-                            <LocationOnIcon style={{ fontSize: 16 }} />
-                            <Typography fontSize="16px" component={'center'}>
-                                Hanoman, Ubud
-                            </Typography>
-                        </div>
+                        <h1 className='title'>Congratulation! <br /> Reward Claimed!</h1>
+                    </Grid>
+                    <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <StarsIcon data-aos="zoom-in" style={{ color:'E8833A', fontSize:'200px' }} />
                     </Grid>
                     <Grid item xs={12}>
-                        <h3 className='title'>You're collected 1 NFT</h3>
-                        <Typography fontSize="15px" component={'center'} style={{ paddingLeft: '20px', paddingRight: '20px' }}>
-                            Purchase any product in the store to get an NFT and get a surprise
-                        </Typography>
-                    </Grid>
-                    <Grid container spacing={2} style={{ padding: '20px' }}>
-                        <Grid item xs={4} sm={6} md={4} lg={3}>
-                            <div className="wrap-image-claim">
-                                <img src={YourIconLogo} alt="Image 1" width="100%" />
-                            </div>
-                        </Grid>
-                        <Grid item xs={4} sm={6} md={4} lg={3}>
-                            <div className="wrap-image-claim">
-                                <img src={YourIconLogo} alt="Image 1" width="100%" />
-                            </div>
-                        </Grid>
-                        <Grid item xs={4} sm={6} md={4} lg={3}>
-                            <div className="wrap-image-claim">
-                                <img src={YourIconLogo} alt="Image 1" width="100%" />
-                            </div>
-                        </Grid>
-                        <Grid item xs={4} sm={6} md={4} lg={3}>
-                            <div className="wrap-image-claim">
-                                <img src={YourIconLogo} alt="Image 1" width="100%" />
-                            </div>
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center', marginTop:'20px', paddingBottom:'20px' }}>
-                        <Button
-                            variant="contained"
-                            className='button-explore'
-                            onClick={() => {
-                                // Add your share functionality here
-                            }}
-                        >
-                           Claim Your Reward
-                        </Button>
+                        <p className='sub-title'>You've claimed 5 NFT from <b>Honney Coffee</b></p>
                     </Grid>
                 </Grid>
             </div>
